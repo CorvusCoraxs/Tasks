dbc9a928c736283887861de39fc3f55bbdfb7098
CorvusCoraxs <84717457+CorvusCoraxs@users.noreply.github.com>
2024-07-08 17:19:10 +0300

    Add files via upload

---
+++ demo1/src/main/java/com/example/demo/Demo1Application.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@SpringBootApplication
+public class Demo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo1Application.class, args);
+        System.out.println("Hello World!");
+    }
+    @Configuration
+    static class SpringJdbcConfig {
+        @Bean
+        public DataSource mysqlDataSource() {
+            DriverManagerDataSource dataSource = new DriverManagerDataSource();
+            dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+            dataSource.setUrl("jdbc:mysql://212.113.123.118:3306/springjdbc");
+            dataSource.setUsername("guest_user");
+            dataSource.setPassword("guest_password");
+
+            return dataSource;
+        }
+    }
+}
