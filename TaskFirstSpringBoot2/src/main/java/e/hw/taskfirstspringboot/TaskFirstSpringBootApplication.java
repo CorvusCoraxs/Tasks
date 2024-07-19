4fa8b61c9dbde07744f0caa1fcf140c973dd1838
YuriPz <yuripz@gmail.com>
2024-07-19 19:37:27 +0300

    - надо добавить кусок, котрый будет инициализировать коннект к БД внутри run()

--- TaskFirstSpringBoot2/src/main/java/e/hw/taskfirstspringboot/TaskFirstSpringBootApplication.java
+++ TaskFirstSpringBoot2/src/main/java/e/hw/taskfirstspringboot/TaskFirstSpringBootApplication.java
@@ -1,14 +1,23 @@
 package e.hw.taskfirstspringboot;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
-public class TaskFirstSpringBootApplication {
-
+public class TaskFirstSpringBootApplication implements CommandLineRunner  {
+    public static final Logger AppThead_log = LoggerFactory.getLogger(TaskFirstSpringBootApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(TaskFirstSpringBootApplication.class, args);
+    }
+
+    public void run(String... args) throws Exception {
+        int i;
+
+        for (i = 0; i < args.length; i++) {}
     }
 
 }
